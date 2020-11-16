951004fe4307147ee20628a14ad8a3b769e8a8b1
FelixFonteneau <34739390+FelixFonteneau@users.noreply.github.com>
2020-11-16 01:55:26 +0100

    Add files via upload

---
+++ Session Key/src/com/company/SessionKey.java
@@ -0,0 +1,22 @@
+package com.company;
+
+import javax.crypto.SecretKey;
+
+public class SessionKey {
+    public SessionKey(Integer keylength){
+
+    }
+
+    public SessionKey(byte[] keybytes){
+
+    }
+
+    public SecretKey getSecretKey(){
+        return null;
+    }
+
+    public byte[] getKeyBytes(){
+        return null;
+    }
+
+}
